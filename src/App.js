910f3c067ac4cfeac5285d92c745bde7e5d01dc0
adhi21pa <adithyapa21@gmail.com>
2024-06-18 19:13:39 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPrescription from './components/AddPrescription';
+import { ViewPrescription } from './components/ViewPrescription';
 
 function App() {
   return (
     <div>
       <AddPrescription/>
+      <ViewPrescription/>
     </div>
   );
 }
