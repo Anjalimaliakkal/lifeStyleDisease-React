2506f223f816755bd9cfd63d31023515feffab48
biji2002 <bijivi7777@gmail.com>
2024-06-18 22:42:58 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPrescription from './components/AddPrescription';
+import PatientSignin from './components/PatientSignin';
 
 function App() {
   return (
     <div>
       <AddPrescription/>
+      <PatientSignin/>
     </div>
   );
 }
