67f92c993ffc98ba97512174b3f74d49c169d664
Anjalimaliakkal <147378299+Anjalimaliakkal@users.noreply.github.com>
2024-06-19 22:09:48 +0530

    Update AddResults.jsx

--- src/components/AddResults.jsx
+++ src/components/AddResults.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react'
 import axios from 'axios'
+import NavBar from './NavBar'
 
 
 const AddResults = () => {
 
     const [data, setdata] = useState(
         {
-            "patientId": "",
+            "name": "",
+            "emailid": "",
             "date": "",
             "result": "",
             "remarks": ""
@@ -19,7 +21,7 @@ const AddResults = () => {
 
     const readValue = () => {
         console.log(data)
-        axios.post("http://localhost:8080/add", data).then((response) => {
+        axios.post("http://localhost:8080/AddResult", data).then((response) => {
             console.log(data)
             if (response.data.status == "success") {
                 alert("Added Successfully")
@@ -32,6 +34,7 @@ const AddResults = () => {
     }
     return (
         <div style={{ backgroundImage: 'url("https://www.liveenhanced.com/wp-content/uploads/2019/02/Best-Online-Doctor-Apps.jpg")', backgroundSize: 'cover', minHeight: '100vh' }}>
+           <NavBar/>
             <center><h1 style={{ color: "#0d6efd" }}>ADD RESULTS</h1></center>
             
             <div className="container">
@@ -40,8 +43,13 @@ const AddResults = () => {
                         <div className="container">
                             <div className="row g-3">
                                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                    <label for="" class="form-label">PATIENT ID</label>
-                                    <input type="text" className="form-control"placeholder='Enter Patient ID'name='patientId' value={data.patientId} onChange={inputHandler}  />
+                                    <label for="" class="form-label">PATIENT NAME</label>
+                                    <input type="text" className="form-control"placeholder='Enter Patient Name'name='name' value={data.name} onChange={inputHandler}  />
+
+                                </div>
+                                <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
+                                    <label for="" class="form-label">PATIENT EMAIL</label>
+                                    <input type="text" className="form-control"placeholder='Enter Patient Email'name='emailid' value={data.emailid} onChange={inputHandler}  />
 
                                 </div>
                                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
@@ -52,8 +60,8 @@ const AddResults = () => {
 
                                 </div>
                                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
-                                    <label htmlFor="" className="form-label">UPLOAD RESULT FILE</label>
-                                    <input type="file"  id="" class="form-control"name='result' value={data.result} onChange={inputHandler} ></input>
+                                    <label htmlFor="" className="form-label">UPLOAD RESULT FILE URL</label>
+                                    <input type="text"  id="" class="form-control"name='resulturl' value={data.resulturl} onChange={inputHandler} ></input>
 
                                 </div>
                                 <div className="col col-12 col-sm-6 col-md-6 col-lg-6 col-xl-6 col-xxl-6">
