7fdcb4c761db107cd52a472eb80e836acc734042
anjalykrishna522002 <anjalykrishnaas@gmail.com>
2024-06-23 10:15:57 +0530

    signIn axios added

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ function App() {
       <ViewPrescription/>
       <AddResults/>
       <SignIn/>
-      <SignUp/>
     </div>
   );
 }
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+
 
 const SignIn = () => {
     const [data, SetData]=useState(
