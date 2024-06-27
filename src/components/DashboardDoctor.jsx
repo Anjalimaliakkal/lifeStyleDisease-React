7fc977813f33c9bf20ec77dbdde0f952712b4212
Anjalimaliakkal <147378299+Anjalimaliakkal@users.noreply.github.com>
2024-06-27 22:11:04 +0530

    Create DashboardDoctor.jsx

---
+++ src/components/DashboardDoctor.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBarDoctor from './NavBarDoctor'
+
+const DashboardDoctor = () => {
+  return (
+    <div>
+      <NavBarDoctor/>
+       <div style={{ backgroundImage: 'url("https://thumbs.dreamstime.com/b/healthcare-technology-doctor-using-digital-tablet-icon-medical-network-hospital-background-162019727.jpg")', backgroundSize: 'cover', minHeight: '100vh' }}></div>
+    </div>
+  )
+}
+
+export default DashboardDoctor
