fd3da0a44cab83b29b64689cd8694b232f9bda46
Anjalimaliakkal <147378299+Anjalimaliakkal@users.noreply.github.com>
2024-06-27 22:21:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPrescription from './components/AddPrescription';
-import { ViewPrescription } from './components/ViewPrescription';
 import AddResults from './components/AddResults';
-import SignIn from './components/SignIn';
-import SignUp from './components/SignUp';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Searchprescription from './components/Searchprescription';
+import SignIndoctor from './components/SignIndoctor';
+import SignUpdoctor from './components/SignUpdoctor';
+import DashboardDoctor from './components/DashboardDoctor';
+import SearchResult from './components/SearchResult';
+import AddFeedback from './components/AddFeedback';
+import ViewFeedback from './components/ViewFeedback';
+import SignUpPatient from './components/SignUpPatient';
+import SignInPatient from './components/SignInPatient';
+import DashboardPatient from './components/DashboardPatient';
+import SearchprescriptionPatient from './components/SearchprescriptionPatient';
+import ViewPatient from './components/ViewPatient';
 
 
 function App() {
   return (
     <div>
-      <AddPrescription/>
-      <ViewPrescription/>
-      <AddResults/>
-      <SignIn/>
-      <SignUp/>
+      <BrowserRouter>
+        <Routes>
+        <Route path='/' element={<SignIndoctor />} />
+          <Route path='/AddPrescription' element={<AddPrescription />} />
+          <Route path='/Searchprescription' element={<Searchprescription />} />
+          <Route path='/AddResults' element={<AddResults />} />
+          <Route path='/SignUpdoctor' element={<SignUpdoctor />} />
+          <Route path='/DashboardDoctor' element={<DashboardDoctor />} />
+          <Route path='/SearchResult' element={<SearchResult />} />
+          <Route path='/AddFeedback' element={<AddFeedback />} />
+          <Route path='/ViewFeedback' element={<ViewFeedback />} />
+          <Route path='/SignUpPatient' element={<SignUpPatient />} />
+          <Route path='/SignInPatient' element={<SignInPatient />} />
+          <Route path='/DashboardPatient' element={<DashboardPatient />} />
+          <Route path='/SearchprescriptionPatient' element={<SearchprescriptionPatient />} />
+          <Route path='/ViewPatient' element={<ViewPatient />} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
